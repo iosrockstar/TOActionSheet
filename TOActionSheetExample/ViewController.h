5ce94199a3ab356e6dfd827ab70d3c2a57f5a1f5
ios rockstar <smellgoodidea@gmail.com>
2016-01-05 19:32:09 +0100

    add TOActionSheet source code

---
+++ TOActionSheetExample/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  TOActionSheetExample
+//
+//  Created by iOS Rockstar on 9/1/15.
+//  Copyright (c) 2015 iOS Rockstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UIButton *lightButton;
+@property (nonatomic, weak) IBOutlet UIButton *darkButton;
+
+@end
+
---
+++ TOActionSheetExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TOActionSheetExample
+//
+//  Created by iOS Rockstar on 9/1/15.
+//  Copyright (c) 2015 iOS Rockstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
